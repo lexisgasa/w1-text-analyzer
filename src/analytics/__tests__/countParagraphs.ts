0a2650bc114c77065f24fffd392660a25b673866
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-07-11 19:00:35 +0200

    Add test to every case

--- src/analytics/__tests__/countParagraphs.ts
+++ src/analytics/__tests__/countParagraphs.ts
@@ -1,7 +1,21 @@
 import { countParagraphs } from "../index.js";
 
 const test1 = (): void => {
-  console.log("countParagraphs function");
+  console.log("\ncountParagraphs should return 0 if no paragraph is given");
+
+  const text = "";
+  const paragraphsCount = countParagraphs(text);
+  const expectedParagraphsCount = 0;
+
+  if (paragraphsCount === expectedParagraphsCount) {
+    console.log("Test passed");
+  } else {
+    console.log("Test failed");
+  }
+};
+
+const test2 = (): void => {
+  console.log("\ncountParagraphs should return 1 if just a paragraph is given");
 
   const text = "Esto es un párrafo";
   const paragraphsCount = countParagraphs(text);
@@ -14,4 +28,27 @@ const test1 = (): void => {
   }
 };
 
+const test3 = (): void => {
+  console.log(
+    "\ncountParagraphs should return the same amount of given paragraphs"
+  );
+
+  const text = `Esto es un párrafo
+  
+  Este es el segundo párrafo
+  
+  Este es el tercer párrafo
+  `;
+  const paragraphsCount = countParagraphs(text);
+  const expectedParagraphsCount = 3;
+
+  if (paragraphsCount === expectedParagraphsCount) {
+    console.log("Test passed");
+  } else {
+    console.log("Test failed");
+  }
+};
+
 test1();
+test2();
+test3();
