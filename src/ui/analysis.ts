9b53b4ed97b05bf08e39ec7bd2074382971b6f6c
Alexis Garcia Saez <lexisgasa@uoc.edu>
2024-07-13 16:50:08 +0200

    Add renderCharactersTotal function

--- src/ui/analysis.ts
+++ src/ui/analysis.ts
@@ -1,4 +1,8 @@
-import { countParagraphs, countWords } from "../analytics/index.js";
+import {
+  countCharacters,
+  countParagraphs,
+  countWords,
+} from "../analytics/index.js";
 
 const totalsContainer = document.querySelector(".totals");
 const shortWordsContainer = document.querySelector(".short");
@@ -125,4 +129,5 @@ export const analyzeText = (text: string): void => {
   // Implement the rest of the analysis here
   renderParagraphsTotal(countParagraphs(text));
   renderWordsTotal(countWords(text));
+  renderCharactersTotal(countCharacters(text));
 };
